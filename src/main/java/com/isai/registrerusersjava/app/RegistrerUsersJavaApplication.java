33626b6ea073d3379aaa2c542150106c5d7f6e28
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-01 09:32:54 -0500

    dependencias necesarias

---
+++ src/main/java/com/isai/registrerusersjava/app/RegistrerUsersJavaApplication.java
@@ -0,0 +1,13 @@
+package com.isai.registrerusersjava.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrerUsersJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrerUsersJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isai/registrerusersjava/app/RegistrerUsersJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.registrerusersjava.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrerUsersJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
