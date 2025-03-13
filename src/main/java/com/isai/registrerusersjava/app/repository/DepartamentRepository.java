a3a6844eb5dbd534ec8a8390be0d85f819d4a997
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-13 08:59:14 -0500

    repository para la entidad departamento

---
+++ src/main/java/com/isai/registrerusersjava/app/repository/DepartamentRepository.java
@@ -0,0 +1,10 @@
+package com.isai.registrerusersjava.app.repository;
+
+import com.isai.registrerusersjava.app.models.Departament;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartamentRepository
+extends JpaRepository<Departament, Long> {
+}
