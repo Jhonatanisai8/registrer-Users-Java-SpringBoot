34932a55efaceee929d575956f776919f8e42670
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-13 09:18:05 -0500

    definicion de un metodo para buscar un departamento por su id haciendo consultas nativas sql

--- src/main/java/com/isai/registrerusersjava/app/repository/DepartamentRepository.java
+++ src/main/java/com/isai/registrerusersjava/app/repository/DepartamentRepository.java
@@ -2,9 +2,18 @@ package com.isai.registrerusersjava.app.repository;
 
 import com.isai.registrerusersjava.app.models.Departament;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartamentRepository
-extends JpaRepository<Departament, Integer> {
+        extends JpaRepository<Departament, Integer> {
+
+    @Query(
+            value = "SELECT * FROM departament WHERE departament_id = departamentId",
+            nativeQuery = true
+    )
+    Departament getDepartamentById(@Param("departamentId") Integer departamentId);
+
 }
