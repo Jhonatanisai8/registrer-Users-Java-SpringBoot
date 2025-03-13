e40077700af2d9ea5176e3cb1000c13b8992b1c3
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-03-13 09:00:33 -0500

    service para la entidad departamento, defiendo algunos metodos

---
+++ src/main/java/com/isai/registrerusersjava/app/service/DepartamentService.java
@@ -0,0 +1,11 @@
+package com.isai.registrerusersjava.app.service;
+
+import com.isai.registrerusersjava.app.models.Departament;
+
+import java.util.List;
+
+public interface DepartamentService {
+    List<Departament> getAllDepartaments();
+
+    Departament getDepartamentById(Long id);
+}
